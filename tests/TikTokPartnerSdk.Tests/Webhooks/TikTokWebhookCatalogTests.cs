d92f1a71f047dbc6fb5f8c06597b9f604127d9fd
Oskar Pra <oskarpra18@gmail.com>
2026-05-04 22:49:53 +0700

    feat: add tiktok webhook catalog

---
+++ tests/TikTokPartnerSdk.Tests/Webhooks/TikTokWebhookCatalogTests.cs
@@ -0,0 +1,42 @@
+using FluentAssertions;
+using TikTokPartnerSdk.Generated.Webhooks;
+
+namespace TikTokPartnerSdk.Tests.Webhooks;
+
+public sealed class TikTokWebhookCatalogTests
+{
+    [Fact]
+    public void Catalog_ShouldContainAllKnownWebhookTypes()
+    {
+        TikTokWebhookCatalog.All.Should().HaveCount(39);
+        TikTokWebhookCatalog.All.Select(item => item.Name).Should().Contain([
+            "Order status change",
+            "Product status change",
+            "Seller deauthorization",
+            "Shoppable content posting"
+        ]);
+    }
+
+    [Fact]
+    public void Catalog_ShouldFindOrderStatusChangeByType()
+    {
+        var item = TikTokWebhookCatalog.FindByType(TikTokWebhookType.OrderStatusChange);
+
+        item.Should().NotBeNull();
+        item!.Type.Should().Be(1);
+        item.Name.Should().Be("Order status change");
+        item.Category.Should().Be("Order");
+        item.Fields.Should().Contain(field => field.Path == "order_status");
+    }
+
+    [Fact]
+    public void Catalog_ShouldFindShoppableContentPostingByType()
+    {
+        var item = TikTokWebhookCatalog.FindByType(TikTokWebhookType.ShoppableContentPosting);
+
+        item.Should().NotBeNull();
+        item!.Type.Should().Be(17);
+        item.Category.Should().Be("Affiliate Creator");
+        item.Fields.Should().Contain(field => field.Path == "data.event.type");
+    }
+}
